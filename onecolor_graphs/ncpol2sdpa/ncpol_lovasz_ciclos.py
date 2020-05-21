8d1411d9edec6e902585e8972a7dea7260174db8
ruffolo14 <gabrielruffolo.gr@gmail.com>
2020-05-21 09:41:35 -0300

    4 deleted files and 1 new file created

--- onecolor_graphs/ncpol2sdpa/ncpol_lovasz_ciclos.py
+++ onecolor_graphs/ncpol2sdpa/ncpol_lovasz_ciclos.py
@@ -17,7 +17,7 @@ def get_monomial_indexes(dic):
 
     indices = {}    #Dicionario        
     for vertex in range(n_vertices):
-        indices['A'+str(vertex)] = [vertex] #Isso e uma lista vazia para cada key de indices. 'A'+'B' concatena strings
+        indices['A'+str(vertex)] = [vertex+1] #Isso e uma lista vazia para cada key de indices. 'A'+'B' concatena strings
         for key in monomials_indexes.keys():
             padrao_encontrado = re.match(str(A[vertex])+r'(.+)', key,flags=0) #compara as keys com 'A[vertex]'+'qualquer string' 
             if padrao_encontrado and monomials_indexes[padrao_encontrado.group()] > n_vertices:  #re.match retorna type True se deu match
@@ -61,8 +61,8 @@ variables_indexes = sdp.monomial_index
 indices = get_monomial_indexes(variables_indexes)
 ## vetores de Gram da matriz de momentos
 moment_matrix = sdp.x_mat[0]
-from decompose_gram_vectors import decompose_gram_vectors
-Gram = decompose_gram_vectors(moment_matrix,2,1e-2)
+from functions import decompose_gram_vectors
+Gram = decompose_gram_vectors(moment_matrix)
 ## dicionario com monomio:vetores
 dic_monomials_vectors = {}
 for vertex in range(n_vertices):
@@ -70,9 +70,10 @@ for vertex in range(n_vertices):
     for linha in indices['A'+str(vertex)]:
         dic_monomials_vectors['A'+str(vertex)].append(Gram[linha]) 
 
+
 ## construindo os projetores como matrizes em dicionario monomios:matrizes
 dic_monomials_matrices = {}
-from get_orthogonal_span import get_orthogonal_span
+from functions import get_orthogonal_span
 for key in dic_monomials_vectors:
     span_vectors = get_orthogonal_span(dic_monomials_vectors[key])
     matrix = sum([np.tensordot(vector,vector, axes=0) for vector in span_vectors])
